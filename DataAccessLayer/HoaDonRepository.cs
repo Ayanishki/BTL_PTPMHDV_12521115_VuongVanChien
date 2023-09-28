3253466e85c572d3bab40c24f2ef5ecd4a78a561
Ayanishki <ayanokouji295@gmail.com>
2023-09-28 11:17:06 +0700

    addthem

---
+++ DataAccessLayer/HoaDonRepository.cs
@@ -0,0 +1,23 @@
+﻿using DataModel;
+
+namespace DataAccessLayer
+{
+    public class HoaDonRepository : IHoaDonRepository
+    {
+        private IDatabaseHelper _dbHelper;
+        public HoaDonRepository(IDatabaseHelper dbHelper)
+        {
+            _dbHelper = dbHelper;
+        }
+
+        public HoaDonModel GetDatabyID(int id)
+        {
+            string msgError = "";
+            try
+            {
+                var dt = _dbHelper.ExecuteSProcedureReturnDataTable(out msgError, "sp_hoadon_get_by_id", "@MaHoaDon", id);
+                if (!string.IsNullOrEmpty(msgError))
+                    throw new Exception(msgError);
+        
+    }
+}
---
+++ DataModel/ThongKeKhachModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataModel
+{
+    public class ThongKeKhachModel
+    {
+        public int MaSach { get; set; }
+        public string TenSach { get; set; }
+        public int SoLuong { get; set; }
+        //public Decimal TongGia { get; set; }
+        public DateTime NgayLapHD { get; set; }
+        public string TenKH { get; set; }
+        public string Diachi { get; set; }
+    }
+}
